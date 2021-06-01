0d3ff9238857b347638db0ee800b1a6c1329505a
omerondertola <omerondertola@gmail.com>
2021-06-01 13:37:03 +0300

    Initial commit

---
+++ src/main/java/tr/com/tolaas/spring/sample/api/gateway/FallbackMethodController.java
@@ -0,0 +1,20 @@
+package tr.com.tolaas.spring.sample.api.gateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackMethodController {
+
+    @GetMapping("/userServiceFallBack")
+    public String userServiceFallBackMethod() {
+        return "User Service is taking longer than expected." +
+                "Please try again later";
+    }
+
+    @GetMapping("/departmentServiceFallBack")
+    public String departmentServiceFallBackMethod() {
+        return "Department Service is taking longer than expected." +
+                "Please try again later";
+    }
+}
